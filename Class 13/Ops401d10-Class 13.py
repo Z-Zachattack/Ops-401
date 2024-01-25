276f1ef297b06159b200532b848cdcb186c39295
Zachariah Woodbridge <github@cyberneticvoid.com>
2024-01-24 18:54:02 -0800

    Update Ops401d10-Class 13.py

--- Class 13/Ops401d10-Class 13.py
+++ Class 13/Ops401d10-Class 13.py
@@ -3,12 +3,59 @@
 # Date of latest revision:      09 Jan 2024
 # Purpose:                      Python script that [...]
 
-# Declaration of imports
+# Imports
 
-# Declaration of variables
+import scapy.all as scapy
+from scapy.all import ICMP, IP, sr1, TCP, send
 
-# Declaration of functions
+# Functions 
 
-# Main
+
+
+# Ping Sweeper
+def ping():
+    target_ip = input("Enter the target IP address: ")
+
+    # Perform ICMP ping sweep
+    response = sr1(IP(dst=target_ip) / ICMP(), timeout=1, verbose=0)
+
+    if response is None:
+        print(f"Host {target_ip} is down or unresponsive.")
+    elif int(response.getlayer(ICMP).type) == 3 and int(response.getlayer(ICMP).code) in [1, 2, 3, 9, 10, 13]:
+        print(f"Host {target_ip} is actively blocking ICMP traffic.")
+    else:
+        print(f"Host {target_ip} is responsive, beginning port scan...")
+        # Call port scan function
+        port_scan(target_ip)
+
+# Function to prompt user for port range and call port scan function
+def port_scan(host):
+    port_range_str = input("Enter the port range (e.g., 20-80): ")
+    start_port, end_port = map(int, port_range_str.split("-"))
+    port_range = range(start_port, end_port + 1)
+    print("Scanning Common Ports...")
+
+    for port in port_range:
+        scan_port(host, port)
+# Port Scanner
+def scan_port(host, port):
+    src_port = 22  # Replace with your desired source port
+    response = sr1(IP(dst=host) / TCP(sport=src_port, dport=port, flags="S"), timeout=1, verbose=0)
+
+    if response is None:
+        print(f"Port {port} is filtered.")
+    elif response.haslayer(TCP):
+        if response.getlayer(TCP).flags == 0x12:
+            send(IP(dst=host) / TCP(sport=src_port, dport=port, flags="R"), verbose=0)
+            print(f"Port {port} is open.")
+        elif response.getlayer(TCP).flags == 0x14:
+            print(f"Port {port} is closed.")
+    else:
+        print(f"Port {port} is in an unknown state.")
+
+# Main 
+
+if __name__ == "__main__":
+    ping()
 
 # End
